ee19af52658c4515a5279b219781bf185d0a4b3f
mhmdnab <mhmdnab004@gmail.com>
2024-11-24 23:31:04 +0200

    project first commit

---
+++ mobile_project_52231493/lib/model/todo.dart
@@ -0,0 +1,20 @@
+class ToDo {
+  String? id;
+  String? todoText;
+  bool isDone;
+
+  ToDo({
+    required this.id,
+    required this.todoText,
+    this.isDone = false,
+  });
+
+  static List<ToDo> todoList() {
+    return [
+      ToDo(id: '01', todoText: 'Meeting with manager', isDone: true ),
+      ToDo(id: '02', todoText: 'Buy Groceries', isDone: true ),
+      ToDo(id: '03', todoText: 'Do the dishes', ),
+      ToDo(id: '04', todoText: 'Family time', ),
+    ];
+  }
+}
